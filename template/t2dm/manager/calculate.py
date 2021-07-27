ab6cbc993789086433531f9d547488bfa043e54d
Tiyi Morris <tiyi.morris@ucl.ac.uk>
2021-07-27 16:38:44 +0100

    Add calculate.py

--- template/t2dm/manager/calculate.py
+++ template/t2dm/manager/calculate.py
@@ -1,3 +1,6 @@
-# Developed from https://core.hash.ai/@hash/risk-of-diabetes/2.1.0
+# Develop from https://core.hash.ai/@hash/risk-of-diabetes/2.1.0
 
 import math
+
+# Has to read from the logger and calculate cumulative and average cost, glucose and carbon for all of the pathways.
+# Does net params do this in PyTorch anyway?
